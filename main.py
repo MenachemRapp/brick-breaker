9b748a1513a89abd54b80965072df4086af5b860
Menachem <72875228+againand@users.noreply.github.com>
2021-08-06 11:34:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+import pygame
+
+WINDOW_WIDTH = 700
+WINDOW_HEIGHT = 500
+
+pygame.init()
+
+size = (WINDOW_WIDTH, WINDOW_HEIGHT)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Game")
+
+
+pygame.quit()
+
