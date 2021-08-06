8f0acf65401fa983de5fab342bd88c8c9875d7c7
Menachem <72875228+againand@users.noreply.github.com>
2021-08-06 12:01:53 +0300

    background and image

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 
 import pygame
 
-WINDOW_WIDTH = 700
-WINDOW_HEIGHT = 500
-
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
 pygame.init()
 
 size = (WINDOW_WIDTH, WINDOW_HEIGHT)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Game")
+pygame.display.set_caption("My Game")
+
+WHITE = (255, 255, 255)
+screen.fill(WHITE)
+
+IMAGE='tiles.png'
+img = pygame.image.load(IMAGE)
+screen.blit(img, (0,0))
+pygame.display.flip()
 
 
+
+finish = False
+while not finish:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finish = True
 pygame.quit()
 
